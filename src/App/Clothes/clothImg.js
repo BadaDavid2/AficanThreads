f98d3ae74942a39cec76670b2668f962a1f760b2
Dave2.0 <olaniyibada462@gmail.com>
2023-02-05 13:49:03 +0100

    'ResponsiveIssuesFixed'

--- src/App/Clothes/clothImg.js
+++ src/App/Clothes/clothImg.js
@@ -1,9 +1,5 @@
 export default [
     {
-        id:"1",
-        img:"model1.jpg"
-    },
-    {
         id:"2",
         img:"model2.jpg"
     },
@@ -11,10 +7,10 @@ export default [
         id:"3",
         img:"model3.jpg"
     },
-    // {
-    //     id:"4",
-    //     img:"model4.jpg"
-    // },
+    {
+        id:"4",
+        img:"model4.jpg"
+    },
     {
         id:"5",
         img:"model5.jpg"
@@ -30,18 +26,6 @@ export default [
     {
         id:"8",
         img:"model8.jpg"
-    },
-    // {
-    //     id:"9",
-    //     img:"model9"
-    // },
-    {
-        id:"10",
-        img:"model10.jpg"
-    },
-    {
-        id:"10",
-        img:"model10.jpg"
     },
     {
         id:"10",
